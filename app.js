b48a9e6098105d2da0b78e6ef5dec3b3d2eb04ef
shuii18 <paullibre18@gmail.com>
2022-12-09 13:36:24 +0100

    Module cors added.

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const router = require("./router");
+const cors = require("cors");
 
 var app = express();
+
+const corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -16,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors(corsOptions));
 
 app.use(router);
 
